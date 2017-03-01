9ac087ae80f8124f8deb598b29aed51d85808cd3
Jeremy <barischj@tcd.ie>
2017-03-01 21:42:39 +0000

    First full implementation

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,26 @@
 module Main where
 
-import Lib
+import           Control.Monad                      (void)
+import qualified Graphics.UI.Threepenny             as UI
+import           Graphics.UI.Threepenny.Core        hiding (row)
+import           Graphics.UI.Threepenny.Ext.Flexbox (ChildProps, attach,
+                                                     cFlexGrow,
+                                                     defaultChildProps,
+                                                     defaultParentProps)
 
 main :: IO ()
-main = someFunc
+main = startGUI defaultConfig example
+
+-- |Example of three divs using a flex-grow ratio of 1:2:1
+example :: Window -> UI ()
+example w = void $
+    attach (getBody w) defaultParentProps $ [grow 1, grow 2, grow 1]
+
+-- |Example "foo" div and given flex-grow value.
+grow :: Int -> (UI Element, ChildProps)
+grow n = (foo, defaultChildProps { cFlexGrow = n })
+
+-- |Example "foo" div.
+foo :: UI Element
+foo = UI.div # set UI.text "foo"
+              # set UI.style [("background-color", "red"), ("margin", "8px")]
--- src/Graphics/UI/Threepenny/Ext/Flexbox.hs
+++ src/Graphics/UI/Threepenny/Ext/Flexbox.hs
@@ -1,30 +1,31 @@
 module Graphics.UI.Threepenny.Ext.Flexbox where
 
+import qualified Clay.Common                 as CC
 import           Clay.Display                (Display)
 import qualified Clay.Display                as CD
 import           Clay.Flexbox                (AlignContentValue,
-                                              AlignItemsValue, FlexDirection,
-                                              FlexWrap, JustifyContentValue)
+                                              AlignItemsValue, AlignSelfValue,
+                                              FlexDirection, FlexWrap,
+                                              JustifyContentValue)
 import qualified Clay.Flexbox                as CF
 import           Clay.Property               (unKeys, unPlain, unValue)
+import           Clay.Size                   (LengthUnit, Size)
 import           Clay.Stylesheet             (Rule (Property), runS)
 import           Data.Text                   (unpack)
 import qualified Graphics.UI.Threepenny      as UI
 import           Graphics.UI.Threepenny.Core
 
--- This library was inspired by the guide to Flexbox at:
--- https://css-tricks.com/snippets/css/a-guide-to-flexbox
-
+-- |Convert to Threepenny style.
 class ToStyle a where
   toStyle :: a -> [(String, String)]
-  
--- |Convert a Clay Property to style.
+
+-- |Convert a Clay Property to Threepnny style.
 instance ToStyle Rule where
   toStyle (Property k v) =
     [(unpack $ unPlain $ unKeys k, unpack $ unPlain $ unValue v)]
 
--- |Properties for the parent.
-data Parent = Parent {
+-- |Properties for a parent.
+data ParentProps = ParentProps {
     pDisplay        :: Display
   , pFlexDirection  :: FlexDirection
   , pFlexWrap       :: FlexWrap
@@ -33,7 +34,8 @@ data Parent = Parent {
   , pAlignContent   :: AlignContentValue
 }
 
-instance ToStyle Parent where
+-- |Convert parent properties to Threepenny style.
+instance ToStyle ParentProps where
   toStyle p = concatMap toStyle $ concatMap runS [
         CD.display        $ pDisplay        p
       , CF.flexDirection  $ pFlexDirection  p
@@ -43,9 +45,9 @@ instance ToStyle Parent where
       , CF.alignContent   $ pAlignContent   p
     ]
 
--- |Default properties for the parent.
-parent :: Parent
-parent = Parent {
+-- |Default properties for a parent.
+defaultParentProps :: ParentProps
+defaultParentProps = ParentProps {
     pDisplay        = CD.flex
   , pFlexDirection  = CF.row
   , pFlexWrap       = CF.nowrap
@@ -53,3 +55,56 @@ parent = Parent {
   , pAlignItems     = CF.stretch
   , pAlignContent   = CF.stretch
 }
+
+-- |Properties for a child.
+data ChildProps = ChildProps {
+    cOrder      :: Int
+  , cFlexGrow   :: Int
+  , cFlexShrink :: Int
+  , cFlexBasis  :: Size LengthUnit
+  , cAlignSelf  :: AlignSelfValue
+}
+
+-- |Convert child properties to Threepenny style.
+instance ToStyle ChildProps where
+  toStyle c = concatMap toStyle $ concatMap runS [
+        CF.order      $ cOrder      c
+      , CF.flexGrow   $ cFlexGrow   c
+      , CF.flexShrink $ cFlexShrink c
+      , CF.flexBasis  $ cFlexBasis  c
+      , CF.alignSelf  $ cAlignSelf  c
+    ]
+
+-- |Default properties for a child.
+defaultChildProps :: ChildProps
+defaultChildProps = ChildProps {
+    cOrder      = 1
+  , cFlexGrow   = 0
+  , cFlexShrink = 1
+  , cFlexBasis  = CC.auto
+  , cAlignSelf  = CC.auto
+}
+
+-- |Set Flexbox properties on an element.
+setProps :: ToStyle a => UI Element -> a -> UI Element
+setProps el props = el # set UI.style (toStyle props)
+
+-- |Attach elements to a parent element, with given Flexbox properties applied.
+attach ::
+     UI Element -> ParentProps  -- Parent and its Flexbox properties
+  -> [(UI Element, ChildProps)] -- Children and respective Flexbox properties
+  -> UI Element                 -- Parent with attached children
+attach p pProps cs = do
+  p'  <- setProps p pProps
+  cs' <- mapM (uncurry setProps) cs
+  element p' #+ map element cs'
+
+-- |Attach elements to a parent element with default Flexbox properties.
+row :: UI Element -> [UI Element] -> UI Element
+row p cs = attach p defaultParentProps $ zip cs $ repeat defaultChildProps
+
+-- |Attach elements to a parent element with flex-direction column.
+column :: UI Element -> [UI Element] -> UI Element
+column p cs = attach p defaultParentProps { pFlexDirection = CF.column } $
+  zip cs $ repeat defaultChildProps
+
