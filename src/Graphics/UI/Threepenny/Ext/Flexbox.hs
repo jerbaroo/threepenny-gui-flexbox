d22340c528ee3a5bd98143f1a3101190c7d0ac51
Daniel Mlot <duplode_1@yahoo.com.br>
2021-02-18 01:53:29 -0300

    Combine properties monoidally
    Besides a nicer syntax for setting multiple properties, basing the
    property bundle types upon `Data.Monoid.Last` offers a way to leave
    properties unspecified. That, in turn, makes `modifyFlex`, a combinator
    which preserves unspecified properties, possible.
    
    The point of `defaultFlexProps` is making it possible to reimplement
    `setFlex` while changing the interface as little as possible. The
    `FlexProps` subclass of `ToStyle` it belongs to exists primarily to
    avoid having to supply a meaningless `defaultFlexProps` implementation
    for `Clay.Stylesheet.Rule`.

--- src/Graphics/UI/Threepenny/Ext/Flexbox.hs
+++ src/Graphics/UI/Threepenny/Ext/Flexbox.hs
@@ -16,7 +16,7 @@ module Graphics.UI.Threepenny.Ext.Flexbox (
   order, flexGrow, flexShrink, flexBasis, alignSelf,
 
   -- * Core Functions
-  ToStyle (..), setFlex,
+  ToStyle (..), FlexProps(..), setFlex, modifyFlex,
 
   -- * Useful Abstractions
   flex, flex_p, flex_c, flex_pc
@@ -34,12 +34,20 @@ import           Clay.Property               (unKeys, unPlain, unValue)
 import           Clay.Size                   (LengthUnit, Size)
 import           Clay.Stylesheet             (Rule (Property), runS)
 import           Data.Text                   (unpack)
+import           Data.Monoid                 (Last(..))
+import           Control.Monad               ((<=<))
+import           Data.Maybe                  (mapMaybe)
 import qualified Graphics.UI.Threepenny      as UI
 import           Graphics.UI.Threepenny.Core hiding (column, row)
 
 -- |Convert to Threepenny style.
 class ToStyle a where
   toStyle :: a -> [(String, String)]
+
+-- | A class for bundles of Flexbox properties suitable for application
+--   to 'Elements'.
+class (ToStyle a, Semigroup a) => FlexProps a where
+  defaultFlexProps :: a
 
 -- |Convert a Clay Property to Threepnny style.
 instance ToStyle Rule where
@@ -48,81 +56,131 @@ instance ToStyle Rule where
 
 -- |Properties for a parent.
 data ParentProps = ParentProps {
-    pDisplay        :: Display
-  , pFlexDirection  :: FlexDirection
-  , pFlexWrap       :: FlexWrap
-  , pJustifyContent :: JustifyContentValue
-  , pAlignItems     :: AlignItemsValue
-  , pAlignContent   :: AlignContentValue
+    pDisplay        :: Last Display
+  , pFlexDirection  :: Last FlexDirection
+  , pFlexWrap       :: Last FlexWrap
+  , pJustifyContent :: Last JustifyContentValue
+  , pAlignItems     :: Last AlignItemsValue
+  , pAlignContent   :: Last AlignContentValue
 }
+
+instance Semigroup ParentProps where
+  p1 <> p2 = ParentProps {
+      pDisplay        = pDisplay p1        <> pDisplay p2
+    , pFlexDirection  = pFlexDirection p1  <> pFlexDirection p2
+    , pFlexWrap       = pFlexWrap p1       <> pFlexWrap p2
+    , pJustifyContent = pJustifyContent p1 <> pJustifyContent p2
+    , pAlignItems     = pAlignItems p1     <> pAlignItems p2
+    , pAlignContent   = pAlignContent p1   <> pAlignContent p2
+  }
+
+instance Monoid ParentProps where
+  mempty = ParentProps {
+      pDisplay        = mempty
+    , pFlexDirection  = mempty
+    , pFlexWrap       = mempty
+    , pJustifyContent = mempty
+    , pAlignItems     = mempty
+    , pAlignContent   = mempty
+  }
 
 -- |Convert parent properties to Threepenny style.
 instance ToStyle ParentProps where
-  toStyle p = concatMap toStyle $ concatMap runS [
-        CD.display        $ pDisplay        p
-      , CF.flexDirection  $ pFlexDirection  p
-      , CF.flexWrap       $ pFlexWrap       p
-      , CF.justifyContent $ pJustifyContent p
-      , CF.alignItems     $ pAlignItems     p
-      , CF.alignContent   $ pAlignContent   p
+  toStyle p = concatMap (toStyle <=< runS) $ mapMaybe getLast $ [
+        CD.display        <$> pDisplay        p
+      , CF.flexDirection  <$> pFlexDirection  p
+      , CF.flexWrap       <$> pFlexWrap       p
+      , CF.justifyContent <$> pJustifyContent p
+      , CF.alignItems     <$> pAlignItems     p
+      , CF.alignContent   <$> pAlignContent   p
     ]
 
 -- |Default properties for a parent.
 parentProps :: ParentProps
 parentProps = ParentProps {
-    pDisplay        = CD.flex
-  , pFlexDirection  = CF.row
-  , pFlexWrap       = CF.nowrap
-  , pJustifyContent = CF.flexStart
-  , pAlignItems     = CF.stretch
-  , pAlignContent   = CF.stretch
+    pDisplay        = pure CD.flex
+  , pFlexDirection  = pure CF.row
+  , pFlexWrap       = pure CF.nowrap
+  , pJustifyContent = pure CF.flexStart
+  , pAlignItems     = pure CF.stretch
+  , pAlignContent   = pure CF.stretch
 }
 
-display        x = parentProps { pDisplay        = x }
-flexDirection  x = parentProps { pFlexDirection  = x }
-flexWrap       x = parentProps { pFlexWrap       = x }
-justifyContent x = parentProps { pJustifyContent = x }
-alignItems     x = parentProps { pAlignItems     = x }
-aligContent    x = parentProps { pAlignContent   = x }
+instance FlexProps ParentProps where
+  defaultFlexProps = parentProps
+
+display        x = mempty { pDisplay        = pure x }
+flexDirection  x = mempty { pFlexDirection  = pure x }
+flexWrap       x = mempty { pFlexWrap       = pure x }
+justifyContent x = mempty { pJustifyContent = pure x }
+alignItems     x = mempty { pAlignItems     = pure x }
+aligContent    x = mempty { pAlignContent   = pure x }
 
 -- |Properties for a child.
 data ChildProps = ChildProps {
-    cOrder      :: Int
-  , cFlexGrow   :: Int
-  , cFlexShrink :: Int
-  , cFlexBasis  :: Size LengthUnit
-  , cAlignSelf  :: AlignSelfValue
+    cOrder      :: Last Int
+  , cFlexGrow   :: Last Int
+  , cFlexShrink :: Last Int
+  , cFlexBasis  :: Last (Size LengthUnit)
+  , cAlignSelf  :: Last AlignSelfValue
 }
+
+instance Semigroup ChildProps where
+  c1 <> c2 = ChildProps {
+      cOrder      = cOrder c1      <> cOrder c2
+    , cFlexGrow   = cFlexGrow c1   <> cFlexGrow c2
+    , cFlexShrink = cFlexShrink c1 <> cFlexShrink c2
+    , cFlexBasis  = cFlexBasis c1  <> cFlexBasis c2
+    , cAlignSelf  = cAlignSelf c1  <> cAlignSelf c2
+  }
+
+instance Monoid ChildProps where
+  mempty = ChildProps {
+      cOrder      = mempty
+    , cFlexGrow   = mempty
+    , cFlexShrink = mempty
+    , cFlexBasis  = mempty
+    , cAlignSelf  = mempty
+  }
 
 -- |Convert child properties to Threepenny style.
 instance ToStyle ChildProps where
-  toStyle c = concatMap toStyle $ concatMap runS [
-        CF.order      $ cOrder      c
-      , CF.flexGrow   $ cFlexGrow   c
-      , CF.flexShrink $ cFlexShrink c
-      , CF.flexBasis  $ cFlexBasis  c
-      , CF.alignSelf  $ cAlignSelf  c
+  toStyle c = concatMap (toStyle <=< runS) $ mapMaybe getLast $ [
+        CF.order      <$> cOrder      c
+      , CF.flexGrow   <$> cFlexGrow   c
+      , CF.flexShrink <$> cFlexShrink c
+      , CF.flexBasis  <$> cFlexBasis  c
+      , CF.alignSelf  <$> cAlignSelf  c
     ]
 
 -- |Default properties for a child.
 childProps :: ChildProps
 childProps = ChildProps {
-    cOrder      = 1
-  , cFlexGrow   = 0
-  , cFlexShrink = 1
-  , cFlexBasis  = CC.auto
-  , cAlignSelf  = CC.auto
+    cOrder      = pure 1
+  , cFlexGrow   = pure 0
+  , cFlexShrink = pure 1
+  , cFlexBasis  = pure CC.auto
+  , cAlignSelf  = pure CC.auto
 }
 
-order      x = childProps { cOrder      = x }
-flexGrow   x = childProps { cFlexGrow   = x }
-flexShrink x = childProps { cFlexShrink = x }
-flexBasis  x = childProps { cFlexBasis  = x }
-alignSelf  x = childProps { cAlignSelf  = x }
+instance FlexProps ChildProps where
+  defaultFlexProps = childProps
 
--- |Set Flexbox properties on an element.
-setFlex :: ToStyle a => a -> UI Element -> UI Element
-setFlex props el = el # set UI.style (toStyle props)
+order      x = mempty { cOrder      = pure x }
+flexGrow   x = mempty { cFlexGrow   = pure x }
+flexShrink x = mempty { cFlexShrink = pure x }
+flexBasis  x = mempty { cFlexBasis  = pure x }
+alignSelf  x = mempty { cAlignSelf  = pure x }
+
+-- | Set Flexbox properties on an element. Unspecified properties are
+--   set to the corresponding 'defaultFlexProps' field values.
+setFlex :: FlexProps a => a -> UI Element -> UI Element
+setFlex props = modifyFlex (defaultFlexProps <> props)
+
+-- | Modify Flexbox properties on an element. Unspecified properties
+--   are left unchanged.
+modifyFlex :: FlexProps a => a -> UI Element -> UI Element
+modifyFlex props el = el # set UI.style (toStyle props)
 
 -- |Attach elements to a parent element, applying given Flexbox properties.
 flex ::
